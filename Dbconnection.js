6314907435b99210aa47dd4cd11e6d4c4b068fb1
khanhthw00 <khanhthudg@gmail.com>
2022-09-03 10:54:18 +0700

    update đbconnection host

--- Dbconnection.js
+++ Dbconnection.js
@@ -1,9 +1,11 @@
 var mysql = require('mysql');
 var connection = mysql.createPool({
-    host: 'localhost',
+    // host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
     password: 'khanhthu.kute2607',
     database: 'doanthuctap',
+    socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
     multipleStatements: true
 });
 module.exports = connection;
