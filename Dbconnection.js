ea8a24e077af29bcb88a20e8885c143be7f77624
khanhthw00 <khanhthudg@gmail.com>
2022-09-05 00:17:41 +0700

    update new host new db in dbconnection

--- Dbconnection.js
+++ Dbconnection.js
@@ -1,12 +1,16 @@
 var mysql = require('mysql');
 var connection = mysql.createPool({
-    host: 'localhost',
+    // host: 'localhost',
 
     // host: 'https://gear-api-project.herokuapp.com',
-    user: 'root',
-    password: 'khanhthu.kute2607',
-    database: 'doanthuctap',
+    // user: 'root',
+    // password: 'khanhthu.kute2607',
+    // database: 'doanthuctap',
     // address: "/Applications/MAMP/tmp/mysql/mysql.sock",
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'bffe6f234ab20c',
+    password: '77ffa67e',
+    database: 'heroku_b08a07c6ad90157',
     multipleStatements: true
 });
 module.exports = connection;
